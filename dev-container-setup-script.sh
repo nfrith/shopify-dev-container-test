b21063c86970f798eb8500a226c4b52fbf7eea89
LAPTOP-MSHURDUV\nfrit <nfrith001@gmail.com>
2024-05-12 05:57:09 +0000

    shopify initialization

---
+++ dev-container-setup-script.sh
@@ -0,0 +1,17 @@
+
+sudo apt-get update
+
+if command -v nvm &> /dev/null; then
+  echo "NVM is installed"
+else
+  echo "NVM is not installed"
+    sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+fi
+
+sudo nvm install 20.12.2
+
+sudo nvm use 20.12.2
+
+sudo npm install -g @shopify/cli
