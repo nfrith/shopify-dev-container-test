5e5d5aa2375fc6025159ee2858224749d53d9a16
LAPTOP-MSHURDUV\nfrit <nfrith001@gmail.com>
2024-05-12 06:15:52 +0000

    config update

--- dev-container-setup-script.sh
+++ dev-container-setup-script.sh
@@ -5,10 +5,12 @@ if command -v nvm &> /dev/null; then
   echo "NVM is installed"
 else
   echo "NVM is not installed"
-    sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 fi
+
+source ~/.bashrc
 
 sudo nvm install 20.12.2
 
